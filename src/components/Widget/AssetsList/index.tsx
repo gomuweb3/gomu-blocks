c23acfb9c9dc63a6872418a68c93ca77e03fc7cb
kainanaina <talanovdev@gmail.com>
2022-06-10 17:10:37 +0800

    add infinite scrolling to AssetsList

--- src/components/Widget/AssetsList/index.tsx
+++ src/components/Widget/AssetsList/index.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect, useContext } from 'react';
-import { useQuery } from 'react-query';
+import { useState, useEffect, useContext, Fragment } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { useInView } from 'react-intersection-observer';
 import cn from 'classnames';
 import { getWalletAssets, SUPPORTED_NFT_TYPES } from 'src/api';
 import { getAssetId, getImgFromAsset, rangeFromZero } from 'src/utils';
@@ -7,6 +8,8 @@ import { NftAsset } from 'src/typings';
 import { WidgetContext } from '../context';
 import { PrimitiveAsset } from '../types';
 import s from './styles.module.scss';
+
+const ASSETS_LIMIT = 50;
 
 const AssetsList = ({
   selectedIds,
@@ -23,12 +26,35 @@ const AssetsList = ({
 }) => {
   const { userAddress, chainId } = useContext(WidgetContext)!;
   const [selectedAssets, setSelectedAssets] = useState<PrimitiveAsset[]>([]);
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
 
-  const { data: walletAssets, isLoading: assetsLoading } = useQuery(
+  const {
+    data: walletAssets,
+    isLoading: assetsLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
     ['walletAssets', userAddress, chainId],
-    () => getWalletAssets({ address: userAddress, includeNonStandardTokenTypes: true }),
-    { enabled: !!userAddress },
+    ({ pageParam = '' }) => getWalletAssets({
+      address: userAddress,
+      includeNonStandardTokenTypes: true,
+      limit: ASSETS_LIMIT,
+      cursor: pageParam,
+    }),
+    {
+      getNextPageParam: (lastPage) => lastPage.data.length === ASSETS_LIMIT ? lastPage.nextCursor : undefined,
+      enabled: !!userAddress
+    },
   );
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage]);
 
   const convertToPrimitiveAsset = (asset: NftAsset) => {
     const id = getAssetId(asset);
@@ -39,14 +65,16 @@ const AssetsList = ({
 
   useEffect(() => {
     if (selectedIds && selectedIds.length !== selectedAssets.length) {
-      const newAssets = walletAssets?.data.reduce((acc, asset) => {
-        const id = getAssetId(asset);
-        if (!selectedIds.includes(id)) {
+      const newAssets = walletAssets?.pages.reduce((acc, { data }) => {
+        const filteredAssets = data.filter((asset) => {
+          const id = getAssetId(asset);
+          return selectedIds.includes(id);
+        });
+        if (!filteredAssets.length) {
           return acc;
         }
 
-        acc.push(convertToPrimitiveAsset(asset));
-        return acc;
+        return acc.concat(filteredAssets.map(convertToPrimitiveAsset));
       }, [] as PrimitiveAsset[]) || [];
       setSelectedAssets(newAssets);
     }
@@ -70,21 +98,29 @@ const AssetsList = ({
     onSelect?.(newSelectedAssets);
   };
 
-  return (
-    <div className={cn(s.assets, { [s._static]: isStatic, [s._compact]: isCompact })}>
-      {assetsLoading
-        ? rangeFromZero(6).map((index) => {
-          return (
-            <div
-              key={index}
-              className={cn(s.assetsItem, 'g-processing')}
-            >
-              <div className={s.assetsItemImg} />
-              <div className={s.assetsItemContent} />
-            </div>
-          );
-        })
-        : walletAssets?.data.map((asset) => {
+  const renderPlaceholders = () => {
+    return rangeFromZero(6).map((index) => {
+      return (
+        <div
+          key={`placeholder-${index}`}
+          className={cn(s.assetsItem, 'g-processing')}
+        >
+          <div className={s.assetsItemImg} />
+          <div className={s.assetsItemContent} />
+        </div>
+      );
+    });
+  };
+
+  const renderAssets = () => {
+    if (assetsLoading) {
+      return renderPlaceholders();
+    }
+
+    return walletAssets?.pages.map((page, i) => {
+      return (
+        <Fragment key={i}>
+          {page.data.map((asset) => {
             const id = getAssetId(asset);
             const img = getImgFromAsset(asset);
             const tokenId = `#${asset.tokenId}`;
@@ -112,8 +148,17 @@ const AssetsList = ({
                 </div>
               </div>
             );
-          })
-      }
+          })}
+        </Fragment>
+      );
+    });
+  };
+
+  return (
+    <div className={cn(s.assets, { [s._static]: isStatic, [s._compact]: isCompact })}>
+      {renderAssets()}
+      {isFetchingNextPage && renderPlaceholders()}
+      {hasNextPage && <div className={s.assetsScrollTrigger} ref={ref} />}
     </div>
   );
 };
