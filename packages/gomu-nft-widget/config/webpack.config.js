551ab7a5a263fa40343d29daae37bb206c1328c9
yongwee <yongwee1@gmail.com>
2022-06-22 13:06:32 +0800

    build: limit max chunks to 1
    - not sure how to completely turn off chunking; chunking happens for walletconnect bundle and coinbase wallet bundle

--- packages/gomu-nft-widget/config/webpack.config.js
+++ packages/gomu-nft-widget/config/webpack.config.js
@@ -742,6 +742,7 @@ module.exports = function (webpackEnv) {
             },
           },
         }),
+      new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
